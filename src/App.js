f6742861ffa2600d816f999aae3f109c7cd4f9a0
Tomiogunsan <tomiafolayan@gmail.com>
2022-08-14 15:34:02 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -8,26 +8,21 @@ import Contact from './component/Contact'
 
 import Home from './component/Home';
 import Navbar from './component/navBar/Navbar';
-import Particles from 'react-tsparticles';
-import {loadFull} from 'tsparticles';
-import particles from './assets/particles';
+
 
 function App() {
 
   const location = useLocation();
   console.log(location); 
 
-const handleinit = async (main) =>(
-  await loadFull(main)
-)
 
-const renderParticleJsInHomePage = location.pathname === '/';
+// const renderParticleJsInHomePage = location.pathname === '/';
 
 
   return (
     <div className={classes.app}>
       {/*particles js*/}
-       { renderParticleJsInHomePage && (<Particles  id='particles' options={particles} init={handleinit}/>)}
+       {/* { renderParticleJsInHomePage && (<App />)} */}
 
        {/* navbar*/}
        <Navbar />
